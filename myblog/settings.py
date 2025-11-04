562eba5f3513d9a682917c7c3a63508c317a0010
Yarielsy <yarigonza0294@gmail.com>
2025-11-04 13:00:42 -0500

    Autenticación y perfiles de usuario, CRUD completo de posts.

--- myblog/settings.py
+++ myblog/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "blog",
+    "ckeditor"
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,17 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Configuración de archivos multimedia (subidos por usuarios)
+MEDIA_URL = '/media/'              # URL para acceder a los archivos
+MEDIA_ROOT = BASE_DIR / 'media'    # Carpeta física donde se guardan
+
+# Default primary key field type
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGIN_URL = 'blog:login'
+LOGIN_REDIRECT_URL = 'blog:post_list'
+LOGOUT_REDIRECT_URL = 'blog:post_list'
